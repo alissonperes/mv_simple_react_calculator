b326de4de49a9994cd529824cd055b413ac7ead6
Alisson Peres <aperes.dev@gmail.com>
2020-05-12 12:46:20 -0300

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -7,25 +7,25 @@ function App() {
       <div className="visor">0</div>
       <div className="calculator">
         <div className="keypad-row">
-          <button>AC</button>
-          <button>+/-</button>
-          <button>%</button>
-          <button>÷</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>x</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
-          <button className="double-space">0</button>
-          <button>.</button>
-          <button>=</button>
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button">÷</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button">x</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button">-</button>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button">+</button>
+          <button type="button" className="double-space">0</button>
+          <button type="button">.</button>
+          <button type="button">=</button>
         </div>
       </div>
     </div>
