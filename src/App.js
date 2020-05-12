5238211b85353d4e299bcae00c1ed67443afbd56
Alisson Peres <aperes.dev@gmail.com>
2020-05-12 11:04:16 -0300

    Initial calculator display with jsx

--- src/App.js
+++ src/App.js
@@ -4,7 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="calculator"/>
+      <div className="visor">0</div>
+      <div className="calculator">
+        <div className="keypad-row">
+          <button>AC</button>
+          <button>+/-</button>
+          <button>%</button>
+          <button>÷</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>x</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+          <button className="double-space">0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
     </div>
   );
 }
